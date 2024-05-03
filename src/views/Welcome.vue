c17c709b21bed1b9a253c998df95194761ee1126
harkeetb <harkeet.b07@gmail.com>
2024-05-03 17:01:39 -0500

    Removed boilerplate components

---
+++ src/views/Welcome.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="welcome container">
+    <p>Welcome</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    
+  }
+}
+</script>
+
+<style>
+  .welcome{ 
+    text-align: center;
+    padding: 20px 0;
+  }
+</style>
