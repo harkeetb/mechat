105c91e3351bb8bf6d6952bc4c51908215014fc0
harkeetb <harkeet.b07@gmail.com>
2024-06-11 21:34:10 -0500

    remove unused props param

--- src/components/SignupForm.vue
+++ src/components/SignupForm.vue
@@ -13,7 +13,7 @@ import { ref } from 'vue';
 import useSignup from '@/composables/useSignup';
 
 export default {
-  setup(props, context) {
+  setup(context) {
     const { error, signup } = useSignup();
 
     // refs
@@ -32,7 +32,3 @@ export default {
   }
 }
 </script>
-
-<style>
-
-</style>
